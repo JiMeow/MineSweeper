7bccbf8f7e536243928d009b952d3100c08110d2
jiratQ <jirat123@icloud.com>
2022-12-01 13:58:38 +0700

    update setting scene for set music and sfx

--- Assets/Scripts/Setting/SoundManager.cs
+++ Assets/Scripts/Setting/SoundManager.cs
@@ -16,6 +16,15 @@ public class SoundManager : MonoBehaviour
     [SerializeField]
     private AudioSource ButtonClickSound;
 
+    private float bgSoundVolume;
+    private float loseSoundVolume;
+    private float winSoundVolume;
+    private float clickSoundVolume;
+    private float buttonClickSoundVolume;
+
+    private float musicMul;
+    private float soundMul;
+
     private void Awake()
     {
         if (instance == null)
@@ -27,6 +36,13 @@ public class SoundManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        bgSoundVolume = 0.075f;
+        loseSoundVolume = 0.2f;
+        winSoundVolume = 0.4f;
+        clickSoundVolume = 0.075f;
+        buttonClickSoundVolume = 0.5f;
+        musicMul = 1f;
+        soundMul = 1f;
     }
 
     public void PlayLoseSound()
@@ -47,5 +63,20 @@ public class SoundManager : MonoBehaviour
     public void PlayButtonClickSound()
     {
         ButtonClickSound.Play();
+    }
+
+    public void SetMusic(float mul)
+    {
+        musicMul = mul;
+        BgSound.volume = bgSoundVolume * musicMul;
+    }
+
+    public void SetSound(float mul)
+    {
+        soundMul = mul;
+        LoseSound.volume = loseSoundVolume * soundMul;
+        WinSound.volume = winSoundVolume * soundMul;
+        ClickSound.volume = clickSoundVolume * soundMul;
+        ButtonClickSound.volume = buttonClickSoundVolume * soundMul;
     }
 }
---
+++ Assets/Scripts/Setting/SoundSetting.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSetting : MonoBehaviour
+{
+    Slider musicSlider;
+    Slider soundSlider;
+
+    private void Start()
+    {
+        musicSlider = GameObject.Find("MusicSlider").GetComponent<Slider>();
+        soundSlider = GameObject.Find("SFXSlider").GetComponent<Slider>();
+    }
+
+    public void SetMusic()
+    {
+        SoundManager.instance.SetMusic(musicSlider.value);
+    }
+
+    public void SetSound()
+    {
+        SoundManager.instance.SetSound(soundSlider.value);
+    }
+}
